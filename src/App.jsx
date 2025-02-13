65b6b300253815c1a9aadbd93bbca72fa5bc8cd0
kimse8699 <kimse8699@gmail.com>
2025-02-13 12:46:42 +0900

    0213공부

--- src/App.jsx
+++ src/App.jsx
@@ -39,47 +39,21 @@ function App() {
             </div>
           </div>
         }/>
-        <Route path='/detail' element={<Detail></Detail>} />
-        <Route path='/about' element={<About />}>
-          <Route path='member' element={<div>멤버임</div>} />
-          <Route path='location' element={<div>위치정보임</div>} />
-        </Route>
-          <Route path='/event' element={<Event></Event>}>
-          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
-          <Route path='two' element={<div>생일기념 쿠폰받기기</div>} />
-        </Route>
+        <Route path='/detail/:id' element={<Detail shoes={shoes}></Detail>} />
       </Routes>
 
     </div>
   )
 }
 
-function Event(){
-  return (
-    <div>
-      <h1>오늘의 이벤트</h1>
-      <Outlet></Outlet>
-    </div>
-  )
-}
-
-function About(){
-  return (
-    <div>
-      <h4>회사정보임</h4>
-      <Outlet></Outlet>
-    </div>
-  )
-}
-
 function Card(props) {
-    return (
-      <div className='col-md-4'>
-        <img src={props.data.img_url} width='80%'></img>
-        <h4>{props.data.title}</h4>
-        <p>{props.data.price}</p>
-      </div>
-    )
+  return (
+    <div className='col-md-4'>
+      <img src={props.data.img_url} width='80%'></img>
+      <h4>{props.data.title}</h4>
+      <p>{props.data.price}</p>
+    </div>
+  )
 }
 
 export default App
--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,20 +1,41 @@
-function Detail() {
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import styled from 'styled-components'
 
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-          </div>
-          <div className="col-md-6">
-            <h4 className="pt-5">상품명</h4>
-            <p>상품설명</p>
-            <p>120000원</p>
-            <button className="btn btn-danger">주문하기</button> 
-          </div>
+function Detail(props) {
+
+  let {id} = useParams(); 
+
+  useEffect(()=>{
+    setTimeout(() => {
+      
+    }, 2);
+  })
+
+  return (
+    <div className="container">
+      <Discount></Discount>
+      <div className="row">
+        <div className="col-md-6">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{props.shoes.find(shoes => shoes.id == id).title}</h4>
+          <p>{props.shoes.find(shoes => shoes.id == id).content}</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button> 
         </div>
       </div>
-    ) 
-  }
+    </div>
+  ) 
+}
 
 export default Detail;
+
+function Discount(){
+  return (
+    <div className="alert alert-warning">
+      2초 이내 구매시 할인
+    </div>
+  )
+}
